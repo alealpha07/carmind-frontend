e5a9600804261f371238d37bfba3d857a5c6a513
Armando <armando.scuotto.studenti@isii.it>
2025-02-11 16:04:31 +0100

    added props for data form component

--- src/lib/components/DataForm.svelte
+++ src/lib/components/DataForm.svelte
@@ -1,35 +1,43 @@
 <script lang="ts">
-	let {} = $props();
+	let { data = ['test', '2024-02-22'], fields = [{ type: 'text', label: 'label' }, {type: 'date', label: 'label2', min: '2023-01-01', max: '2025-12-31'}] } = $props();
 </script>
 
 <div id="form-container">
 	<div class="row">
-    <div class="col-12" style="margin-top: 5px;">
-      <label for="dateElement"><b>DateField</b></label> <br>
-      <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31"/>
-    </div>
-    <div class="col-12" style="margin-top: 5px;">
-      <label for="textElement"><b>TextField</b></label> <br>
-      <input type="text" id="textElement" placeholder="test" />
-    </div>
-    <div class="col-12" style="margin-top: 5px;">
-      <label for="numberElement"><b>NumberField</b></label> <br>
-      <input type="number" id="numberElement" placeholder="test" />
-    </div>
-    <div class="col-12" style="margin-top: 5px;">
-      <div class="align-container">
-        <b>BoolField</b>
-        <label style="display: inline-block;" class="switch" for="boolElement">
-          <input type="checkbox" id="boolElement" />
-          <div class="slider round"></div>
-        </label>
-      </div>
-    </div>
-    <div class="col-12" style="margin-top: 5px;">
-      <label for="fileElement"><b>FileField</b></label> <br>
-      <input type="file" id="fileElement" />
-    </div>
-  </div>
+		{#each fields as field, index}
+      {#if field.type == 'text'}
+			<div class="col-12" style="margin-top: 5px;">
+				<label for="form-element-{index}"><b>{field.label}</b></label> <br />
+				<input type="text" id="form-element-{index}" placeholder="{field.label}" value="{data[index]}" />
+			</div>
+      {:else if field.type == 'date'}
+			<div class="col-12" style="margin-top: 5px;">
+				<label for="form-element-{index}"><b>{field.label}</b></label> <br />
+				<input type="date" id="form-element-{index}" value="{data[index]}" min="{field.min}" max="{field.max}" />
+			</div>
+      {:else if field.type == 'number'}
+			<div class="col-12" style="margin-top: 5px;">
+				<label for="numberElement"><b>NumberField</b></label> <br />
+				<input type="number" id="numberElement" placeholder="test" />
+			</div>
+      {:else if field.type == 'boolean'}
+			<div class="col-12" style="margin-top: 5px;">
+				<div class="align-container">
+					<b>BoolField</b>
+					<label style="display: inline-block;" class="switch" for="boolElement">
+						<input type="checkbox" id="boolElement" />
+						<div class="slider round"></div>
+					</label>
+				</div>
+			</div>
+      {:else if field.type == 'file'}
+			<div class="col-12" style="margin-top: 5px;">
+				<label for="fileElement"><b>FileField</b></label> <br />
+				<input type="file" id="fileElement" />
+			</div>
+      {/if}
+		{/each}
+	</div>
 </div>
 
 <style>
@@ -45,9 +53,9 @@
 		gap: 8px;
 	}
 
-  input{
-    width: 100%;
-  }
+	input {
+		width: 100%;
+	}
 
 	.switch {
 		display: inline-block;
