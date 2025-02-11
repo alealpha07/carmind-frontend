3d721d475b6d6d6939ee23b95eef8682c52f2e49
Armando <armando.scuotto.studenti@isii.it>
2025-02-11 15:29:53 +0100

    added dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    let {show = $bindable(), children, ...others} = $props();
+
+</script>
+
+<dialog open={show} {...others}>
+    {@render children()};
+</dialog>
+
+
+<style>
+    dialog {
+        position: absolute;
+        border: none;
+        background-color: var(--color-bg-3);
+        border-radius: 15px;
+        box-shadow: 10px 10px 10px var(--color-bg-5);
+        padding: 15px;
+        margin: auto;
+        width: 50%;
+        height: 50%;
+    }
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
-	import DataForm from '$lib/components/DataForm.svelte';
+    import DataForm from '$lib/components/DataForm.svelte';
+    import Dialog from '$lib/components/Dialog.svelte';
+
+    let showDialog = $state(false);
+
+    function toggleDialog(){
+        showDialog = !showDialog;
+    }
 </script>
 
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+    <title>About</title>
+    <meta name="description" content="About this app" />
 </svelte:head>
 
 <DataForm></DataForm>
+<button onclick={toggleDialog}>toggle</button>
+<Dialog bind:show={showDialog}><p>prova</p></Dialog>
